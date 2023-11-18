2f52b6be1cbd1c22bb744429f59488109ec08a8b
rwlush <rwlush@mun.ca>
2023-11-18 16:05:03 -0330

    -Added cloud firestore -Removed hive dependencies

--- lib/model/diary_entry.dart
+++ lib/model/diary_entry.dart
@@ -1,14 +1,7 @@
-import 'package:hive/hive.dart';
-
-part 'diary_entry.g.dart'; // Name of the generated file
-
-@HiveType(typeId: 0) // Unique typeId for Hive
+import 'package:cloud_firestore/cloud_firestore.dart';
 class DiaryEntry {
-  @HiveField(0)
   final DateTime date;
-  @HiveField(1)
   final String description;
-  @HiveField(2)
   final int rating;
 
   DiaryEntry(this.date, this.description, this.rating);

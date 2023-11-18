ab677b0b8356c81f07e4ff18d600a8f1e15077ed
rwlush <rwlush@mun.ca>
2023-11-18 16:19:01 -0330

    -Set up model class for DiaryEntry -Refactored to remove hive dependencies and added cloud firestore -Added helper functions to convert between map and firestore document

--- lib/model/diary_entry.dart
+++ lib/model/diary_entry.dart
@@ -1,8 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 class DiaryEntry {
-  final DateTime date;
+  final String? id;
+  final Timestamp date;
   final String description;
   final int rating;
 
-  DiaryEntry(this.date, this.description, this.rating);
+  DiaryEntry(
+      {this.id,
+      required this.date,
+      required this.description,
+      required this.rating});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'date': date,
+      'description': description,
+      'rating': rating,
+    };
+  }
+
+  static DiaryEntry fromMap(DocumentSnapshot doc) {
+    Map<String, dynamic> map = doc.data() as Map<String, dynamic>;
+    return DiaryEntry(
+      id: doc.id,
+      date: map['date'] as Timestamp,
+      description: map['description'],
+      rating: map['rating'],
+    );
+  }
 }

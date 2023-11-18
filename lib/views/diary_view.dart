0d084db6b21b840f1fef1a6493e97b65e6f408ab
rwlush <rwlush@mun.ca>
2023-11-18 17:52:11 -0330

    -Updated diary view page with StreamBuilder and Firebase logic

--- lib/views/diary_view.dart
+++ lib/views/diary_view.dart
@@ -1,7 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dear_diary/views/add_entry_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:intl/intl.dart';
 import '../controller/diary_controller.dart';
 import '../model/diary_entry.dart';
@@ -20,7 +20,12 @@ class DiaryView extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              Navigator.pushNamed(context, '/addEntry');
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => AddEntryView(diaryController: diaryController)
+                ),
+              );
             },
           ),
           IconButton(
@@ -31,62 +36,66 @@ class DiaryView extends StatelessWidget {
           ),
         ],
       ),
-      // body: ValueListenableBuilder(
-      //   valueListenable: diaryController.diaryBox.listenable(),
-      //   builder: (context, Box box, widget) {
-      //     if (box.isEmpty) {
-      //       return const Center(
-      //         child: Text('No entries found'),
-      //       );
-      //     } else {
-      //       final entries = box.values.cast<DiaryEntry>().toList();
-      //       entries.sort((a, b) => b.date
-      //           .compareTo(a.date)); // Sort entries by date in descending order
-      //       return Padding(
-      //         padding: const EdgeInsets.all(16.0),
-      //         child: ValueListenableBuilder(
-      //           valueListenable: diaryController.diaryBox.listenable(),
-      //           builder: (context, Box box, widget) {
-      //             if (box.isEmpty) {
-      //               return const Center(
-      //                 child: Text('No entries found'),
-      //               );
-      //             } else {
-      //               final entries = box.values.cast<DiaryEntry>().toList();
-      //               entries.sort((a, b) => b.date.compareTo(a.date));
+      body: StreamBuilder(
+        stream: diaryController.diaryEntryCollection.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: Text('No entries found'),
+            );
+          } else {
+            final entries = snapshot.data?.docs
+                .map((doc) => DiaryEntry.fromMap(doc))
+                .toList();
+            entries?.sort((a, b) => b.date
+                .compareTo(a.date)); // Sort entries by date in descending order
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: StreamBuilder(
+                stream: diaryController.diaryEntryCollection.snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: Text('No entries found'),
+                    );
+                  } else {
+                    final entries = snapshot.data?.docs
+                        .map((doc) => DiaryEntry.fromMap(doc))
+                        .toList();
+                    entries?.sort((a, b) => b.date.compareTo(a.date));
 
-      //               List<Widget> widgets = [];
-      //               DateTime? lastDate;
-      //               for (int i = 0; i < entries.length; i++) {
-      //                 final entry = entries[i];
-      //                 if (lastDate == null ||
-      //                     entry.date.month != lastDate.month ||
-      //                     entry.date.year != lastDate.year) {
-      //                   final headerText =
-      //                       DateFormat('MMMM yyyy').format(entry.date);
-      //                   widgets.add(DateHeader(text: headerText));
-      //                 }
-      //                 widgets.add(
-      //                   DiaryEntryWidget(
-      //                     entry: entry,
-      //                     onDelete: () {
-      //                       diaryController.deleteEntryByEntry(entry);
-      //                     },
-      //                   ),
-      //                 );
-      //                 lastDate = entry.date;
-      //               }
-
-      //               return ListView(
-      //                 children: widgets,
-      //               );
-      //             }
-      //           },
-      //         ),
-      //       );
-      //     }
-      //   },
-      // ),
+                    List<Widget> widgets = [];
+                    DateTime? lastDate;
+                    for (int i = 0; i < entries!.length; i++) {
+                      final entry = entries[i];
+                      if (lastDate == null ||
+                          entry.date.month != lastDate.month ||
+                          entry.date.year != lastDate.year) {
+                        final headerText =
+                            DateFormat('MMMM yyyy').format(entry.date);
+                        widgets.add(DateHeader(text: headerText));
+                      }
+                      widgets.add(
+                        DiaryEntryWidget(
+                          entry: entry,
+                          onDelete: () {
+                            diaryController.removeEntry(entry);
+                          },
+                        ),
+                      );
+                      lastDate = entry.date;
+                    }
+                    return ListView(
+                      children: widgets,
+                    );
+                  }
+                },
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
